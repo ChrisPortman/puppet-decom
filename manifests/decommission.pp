50c52a3358082db4cd8be49fe2f057f2ab129ab0
Chris Portman <chris.portman@optusnet.com.au>
2015-09-02 14:01:35 +1000

    release IP addresses

--- manifests/decommission.pp
+++ manifests/decommission.pp
@@ -5,12 +5,35 @@ class decom::decommission {
   #Set node for reinstall in razor
   decom_razor_node($decom::razor_node, 'decommission', $decom::razor_host)
 
-  exec { "DHCP_RELEASE_${decom::certname}":
-    command => '/sbin/dhclient -r',
-  }
-
   #Clean cert
   decom_clean_cert($decom::certname, $::environment, $decom::ca_host)
+
+  # There is inconsistancy with regards to what dhclient files exist.
+  # Test for different combinations and run the ones that apply
+  $eth0_conf_file     = '/etc/dhcp/dhclient-eth0.conf'
+  $eth0_lease_file    = '/var/lib/dhclient/dhclient-eth0.leases'
+  $generic_conf_file  = '/etc/dhcp/dhclient.conf'
+  $generic_lease_file = '/var/lib/dhclient/dhclient.leases'
+
+  exec { "DHCP_RELEASE_${decom::certname}-1":
+    command => "/sbin/dhclient -cf $eth0_conf_file -lf $eth0_lease_file -r",
+    onlyif  => [ "test -f  $eth0_conf_file", "test -f $eth0_lease_file" ],
+  }
+
+  exec { "DHCP_RELEASE_${decom::certname}-2":
+    command => "/sbin/dhclient -cf $generic_conf_file -lf $generic_lease_file -r",
+    onlyif  => [ "test -f  $generic_conf_file", "test -f $generic_lease_file" ],
+  }
+
+  exec { "DHCP_RELEASE_${decom::certname}-3":
+    command => "/sbin/dhclient -cf $generic_conf_file -lf $eth0_lease_file -r",
+    onlyif  => [ "test -f  $generic_conf_file", "test -f $eth0_lease_file" ],
+  }
+
+  exec { "DHCP_RELEASE_${decom::certname}-4":
+    command => "/sbin/dhclient -cf $eth0_conf_file -lf $generic_lease_file -r",
+    onlyif  => [ "test -f  $eth0_conf_file", "test -f $generic_lease_file" ],
+  }
 
   exec { "SHUTDOWN_${decom::certname}":
     command => '/usr/bin/halt --force',
