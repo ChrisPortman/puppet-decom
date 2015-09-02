738a670c01daa478258f596fe61d4e0fceba146e
Chris Portman <chris.portman@optusnet.com.au>
2015-09-02 14:05:28 +1000

    release IP addresses

--- manifests/decommission.pp
+++ manifests/decommission.pp
@@ -18,26 +18,29 @@ class decom::decommission {
   exec { "DHCP_RELEASE_${decom::certname}-1":
     command => "/sbin/dhclient -cf $eth0_conf_file -lf $eth0_lease_file -r",
     onlyif  => [ "test -f  $eth0_conf_file", "test -f $eth0_lease_file" ],
+    before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-2":
     command => "/sbin/dhclient -cf $generic_conf_file -lf $generic_lease_file -r",
     onlyif  => [ "test -f  $generic_conf_file", "test -f $generic_lease_file" ],
+    before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-3":
     command => "/sbin/dhclient -cf $generic_conf_file -lf $eth0_lease_file -r",
     onlyif  => [ "test -f  $generic_conf_file", "test -f $eth0_lease_file" ],
+    before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-4":
     command => "/sbin/dhclient -cf $eth0_conf_file -lf $generic_lease_file -r",
     onlyif  => [ "test -f  $eth0_conf_file", "test -f $generic_lease_file" ],
+    before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "SHUTDOWN_${decom::certname}":
     command => '/usr/bin/halt --force',
-    require => Exec["DHCP_RELEASE_${decom::certname}"],
   }
 }
 
