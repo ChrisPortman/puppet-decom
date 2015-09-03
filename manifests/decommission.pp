bf1b2431f74d11ffc58405a7960cbd4861c58856
Chris Portman <chris.portman@optusnet.com.au>
2015-09-03 14:28:41 +1000

    fix SG puppet master hostnames

--- manifests/decommission.pp
+++ manifests/decommission.pp
@@ -16,28 +16,28 @@ class decom::decommission {
   $generic_lease_file = '/var/lib/dhclient/dhclient.leases'
 
   exec { "DHCP_RELEASE_${decom::certname}-1":
-    command => "/sbin/dhclient -cf $eth0_conf_file -lf $eth0_lease_file -r",
+    command => "dhclient -cf $eth0_conf_file -lf $eth0_lease_file -r",
     path    => "/usr/bin:/usr/sbin:/sbin:/bin",
     onlyif  => [ "test -f  $eth0_conf_file", "test -f $eth0_lease_file" ],
     before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-2":
-    command => "/sbin/dhclient -cf $generic_conf_file -lf $generic_lease_file -r",
+    command => "dhclient -cf $generic_conf_file -lf $generic_lease_file -r",
     path    => "/usr/bin:/usr/sbin:/sbin:/bin",
     onlyif  => [ "test -f  $generic_conf_file", "test -f $generic_lease_file" ],
     before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-3":
-    command => "/sbin/dhclient -cf $generic_conf_file -lf $eth0_lease_file -r",
+    command => "dhclient -cf $generic_conf_file -lf $eth0_lease_file -r",
     path    => "/usr/bin:/usr/sbin:/sbin:/bin",
     onlyif  => [ "test -f  $generic_conf_file", "test -f $eth0_lease_file" ],
     before  => Exec["SHUTDOWN_${decom::certname}"],
   }
 
   exec { "DHCP_RELEASE_${decom::certname}-4":
-    command => "/sbin/dhclient -cf $eth0_conf_file -lf $generic_lease_file -r",
+    command => "dhclient -cf $eth0_conf_file -lf $generic_lease_file -r",
     path    => "/usr/bin:/usr/sbin:/sbin:/bin",
     onlyif  => [ "test -f  $eth0_conf_file", "test -f $generic_lease_file" ],
     before  => Exec["SHUTDOWN_${decom::certname}"],
